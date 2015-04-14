01ec89cc3fe3e61eae527f7b61726eaa643d81e4
livelxw <livelxw@live.cn>
2015-04-14 21:01:31 +0800

    first commit

---
+++ load_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import binarize
+from sklearn.cross_validation import train_test_split
+
+num_labels = 10
+
+
+def get_split_data(test_size=0.33, threshold=75):
+    X, y = get_prepared_xy(threshold)
+    trX, teX, trY, teY = train_test_split(X, y, test_size=test_size)
+    return trX, teX, trY, teY
+
+
+def get_raw_split_data(test_size=0.33):
+    X, y = get_raw_xy()
+    trX, teX, trY, teY = train_test_split(X, y, test_size=test_size)
+    return trX, teX, trY, teY
+
+
+def get_prepared_xy(threshold=75):
+    df = pd.read_csv("../data/train.csv")
+    X = binarize(df.values[:, 1:], threshold=threshold)
+    y = df.values[:, 0]
+    y = np.eye(num_labels)[y, :]
+    return X, y
+
+
+def get_raw_xy():
+    df = pd.read_csv("../data/train.csv")
+    X = df.values[:, 1:]
+    y = df.values[:, 0]
+    y = np.eye(num_labels)[y, :]
+    return X, y
